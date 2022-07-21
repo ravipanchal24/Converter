1b06ac6e0daea62c75bbf13ebedb8f9787f8c8eb
Ravi Panchal <mpravikumar04@gmail.com>
2022-07-22 01:55:49 +0530

    added clock functionality

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
-const date = new Date();
-const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-document.getElementById("currentDateSpan").innerHTML = month[date.getMonth()] + " " + date.getDate() + ", " + date.getFullYear();
+function getDateTime()
+{
+    const date = new Date();
+    const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    document.getElementById("currentDateSpan").innerHTML = month[date.getMonth()] + " " + date.getDate() + ", " + date.getFullYear();
+    
+
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+
+    if(hours <= 12)
+        document.getElementById("timePeriod").innerHTML = "&nbspAM";
+    else
+    document.getElementById("timePeriod").innerHTML = "&nbspPM";
+
+    if(hours < 10 )
+        hours = "0"+hours;
+    if(minutes < 10 )
+        minutes = "0"+minutes;
+    if(seconds < 10 )
+        seconds = "0"+seconds;
+
+    document.getElementById("currentTimeSpan").innerHTML = hours + ":" + minutes + ":" + seconds;    
+}
+
+setInterval(getDateTime, 10);
 function unitChange()
 {
  
